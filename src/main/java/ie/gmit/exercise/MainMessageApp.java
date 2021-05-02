cc566aaa89a5db177f17e5c48a0fe771721fe708
Marlowe <g00325096@gmit.ie>
2021-05-02 23:15:11 +0100

    Dependency Inversion Principal is use

--- src/main/java/ie/gmit/exercise/MainMessageApp.java
+++ src/main/java/ie/gmit/exercise/MainMessageApp.java
@@ -1,14 +1,22 @@
 package ie.gmit.exercise;
 
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.Normalizer;
 
 public class MainMessageApp {
 
 	public static void main(String[] args) throws IOException {
 
-		Message msg = new Message("This is a message");
+		//Formatter formatter = new JSONFormatter();
+		Formatter formatter = new TextFormatter();
+		//PrintWriter writer = new PrintWriter(new FileWriter("test_msg.txt"));
+		PrintWriter writer = new PrintWriter(System.out);
+
+		Message msg = new Message("This is a message to the Prime Minister...lockdown over please");
 		MessagePrinter printer = new MessagePrinter();
-		printer.writeMessage(msg, "test_msg.txt");
+		printer.writeMessage(msg,formatter,writer);
 	}
 
 }
